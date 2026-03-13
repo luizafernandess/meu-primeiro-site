2595dd632fc20c5277cbc46fa4f39d4a5d1140c3
Luiza Fernandes <luizafernandesesc@email.com>
2026-03-12 23:30:08 -0300

    fix mysql ssl

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ const db = mysql.createConnection({
  user: process.env.MYSQLUSER,
  password: process.env.MYSQLPASSWORD,
  database: process.env.MYSQLDATABASE,
- port: process.env.MYSQLPORT
+ port: process.env.MYSQLPORT,
+ ssl: {
+  rejectUnauthorized: false
+ }
 })
 
 // testar conexão
